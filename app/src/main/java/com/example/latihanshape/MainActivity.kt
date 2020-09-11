4fb396f2b60b43cab235419777bd96b32111fc03
Zulka <zulka.ajuab@gmail.com>
2020-09-11 16:08:09 +0700

    Inisal awal

---
+++ app/src/main/java/com/example/latihanshape/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.latihanshape
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+//
+//    val REQUEST_IMAGE_CAPTURE = 1
+//
+//    fun dispatchTakePictureIntent(view: View) {
+//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+//            takePictureIntent.resolveActivity(packageManager)?.also {
+//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+//            }
+//        }
+//    }
+}
